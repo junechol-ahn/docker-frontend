608fd708a0539d694f7c9316c5d80b7319ec055b
junechol ahn <junechol.ahn@gmail.com>
2026-02-22 01:40:10 +0900

    modified text in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I am the One
+          Feature One Branch
         </p>
         <a
           className="App-link"
